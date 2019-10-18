ce594458b714473305597dc83e7e9a7c5b8d5fcb
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-18 02:54:31 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,11 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-requie './config/environment'
-require 'sinatra/activerecord/rake'
+
+configure :development do 
+  set :database,
+  'sqlite3:db/database.db'
+end
+
+
 require './app'
